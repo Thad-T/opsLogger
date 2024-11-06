94dfd78782fca258105af84be4ff99450e8a6abe
Melvin <melvin_teo@hotmail.sg>
2024-11-07 03:05:11 +0800

    Testing Application v1

--- TestingApplication/TestApplication.c
+++ TestingApplication/TestApplication.c
@@ -9,11 +9,11 @@ int SLEEP_TIME = 2000;
 
 const char* print_current_time() {
     time_t now;
-    time(&now); // Get the current time
+    time(&now); 
     struct tm *local = localtime(&now);
 
     static char time_str[20];
-    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", local);
+    strftime(time_str, sizeof(time_str), "%H:%M:%S", local);
 
     return time_str;
 }
@@ -32,7 +32,7 @@ void write_file(const char *filename, const char *content) {
     }
     fprintf(file, "%s", content);
     fclose(file);
-    print_with_timestamp("Data written to file successfully");
+    print_with_timestamp("Data written successfully");
     pthread_mutex_unlock(&file_mutex);
 }
 
@@ -40,12 +40,12 @@ void read_file(const char *filename) {
     pthread_mutex_lock(&file_mutex);
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        perror("Error opening file for reading");
+        perror("Error opening file");
         pthread_mutex_unlock(&file_mutex);
         exit(1);
     }
     char ch;
-    printf("Reading content of %-31s [%s]\n", filename, print_current_time());
+    printf("Contents: %-40s [%s]\n", filename, print_current_time());
     
     while ((ch = fgetc(file)) != EOF) {
         putchar(ch);
@@ -81,8 +81,8 @@ void delete_file(const char *filename) {
 
 void* thread_function(void* arg) {
     const char *filename = (const char*) arg;
-    const char *initial_content = "This is the initial content of the file.\n";
-    const char *additional_content = "This is additional content appended to the file.\n";
+    const char *initial_content = "Initial content\n";
+    const char *additional_content = "Additional content added\n";
 
     // Write initial content to the file
     write_file(filename, initial_content);
@@ -112,14 +112,14 @@ int main() {
 
     // Initialize the mutex
     if (pthread_mutex_init(&file_mutex, NULL) != 0) {
-        perror("Mutex initialization failed");
+        perror("Error initializing mutex");
         return 1;
     }
 
     // Create and start threads
     for (int i = 0; i < 3; i++) {
         if (pthread_create(&threads[i], NULL, thread_function, (void*) filenames[i]) != 0) {
-            perror("Failed to create thread");
+            perror("Error creating thread");
             return 1;
         }
         Sleep(SLEEP_TIME);
@@ -132,6 +132,5 @@ int main() {
 
     pthread_mutex_destroy(&file_mutex);
 
-    print_with_timestamp("All threads have completed their operations.");
     return 0;
 }
