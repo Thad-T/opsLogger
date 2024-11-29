3f09d35d21a38c51090ed03e5dcf047103a1c273
Nydrel2 <dariustan.sg@gmail.com>
2024-11-29 11:31:43 +0800

    Update framework.h

--- main/framework.h
+++ main/framework.h
@@ -6,6 +6,7 @@
 
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX
 // Windows Header Files
 #include <windows.h>
 // C RunTime Header Files
@@ -18,7 +19,6 @@
 #include <strsafe.h>
 #include <commdlg.h>
 // For GUI/csvFormat/Real Time Update
-#define NOMINMAX
 #include <fstream>
 #include <sstream>
 #include <vector>
